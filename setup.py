4603068c95fc08d9107b9ef74dd0de420177fd98
Vladimir Berkutov <vladimir.berkutov@gmail.com>
2016-08-14 16:04:09 +0900

    Update setup.py to provide urls and version

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+__version__ = '1.0'
+
 
 setup(
     name='picalc',
-    version='1.0',
-    packages=find_packages(),
-    url='',
+    version=__version__,
+    packages=['lcalc'],
+    url='https://github.com/dair-targ/lcalc',
+    download_url='https://github.com/dair-targ/lcalc/tarball/%s' % __version__,
     license='GPLv3',
     author='Vladimir Berkutov',
     author_email='vladimir.berkutov@gmail.com',
-    description='Lambda Calculus',
+    description='Lambda Calculus implementation',
 
     install_require=[
         'parsec>=3.0',
